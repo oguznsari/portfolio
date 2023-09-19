6ca81fb81e6fc15f90d8bc8b883308cb039fb31b
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-09-19 20:54:37 +0300

    rewording about page

--- components/about.tsx
+++ components/about.tsx
@@ -19,36 +19,32 @@ const About = () => {
             <p className="mb-3">
                 After graduating with a degree in{" "}
                 <span className="font-medium">Electronics Engineering</span>, I worked 
-                as a Systems Engineer in Istanbul New Airport's A-SMGCS project where I 
-                found the chance to work with international teams and cutting-edge technology.
-                I decided to pursue my passion for programming. I started coding with{" "} 
-                <span className="font-medium">Python</span>{" "}
-                for machine learning projects thanks to Kaggle and courses from Andrew N.G. 
-                in Coursera. My first job in programming was{" "}
-                <span className="font-medium">full-stack web development</span>.{" "}
-                I've mainly used{" "}
-                <span className="font-medium">PHP and MySQL</span>{" "}
-                After 3 years of fun and coding I've changed my project and started to work
-                mainly with{" "}
+                as a Systems Engineer for a period. However, I decided to pursue 
+                my passion for programming, and I began my coding journey with{" "}
+                <span className="font-medium">Python</span>{", "}
+                primarily focusing on machine learning projects through platforms like 
+                Kaggle and online courses such as those by Andrew Ng on Coursera.
+                My initial role in programming was as a full-stack web developer, 
+                where I predominantly used{" "}
+                <span className="font-medium">PHP and MySQL</span>{". "}
+                After three years of exciting and challenging experiences, 
+                I transitioned to a different project, concentrating on technologies like{" "}
                 <span className="font-medium">React, TypeScript, and NestJS{". "}</span>
-                <span className="italic">My favorite part of programming</span> is the
-                problem-solving aspect. I <span className="underline">love</span> the
-                feeling of finally figuring out a solution to a problem. My core stack
-                is{" "}
-                <span className="font-medium">React, Next.js, Node.js, and MongoDB</span>
-                . I am also familiar with TypeScript and Prisma. I am always looking to
-                learn new technologies. I am currently looking for a{" "}
-                <span className="font-medium">full-time position</span> as a software
-                developer.
             </p>
-
             <p>
-                <span className="italic">When I'm not coding</span>, I enjoy playing
-                video games, watching movies, and jogging. I also enjoy{" "}
-                <span className="font-medium">learning new things</span>. I am currently
-                learning about{" "}
-                <span className="font-medium">history and economics</span>. I'm also
-                learning the dynamics of financial markets.
+                <span className="italic">My favorite aspect of programming</span>{" "}
+                is problem-solving. I derive immense satisfaction from tackling 
+                complex issues and finding effective solutions. My core technology stack includes{" "} 
+                <span className="font-medium">React, Next.js, Node.js, and MongoDB</span>{". "}
+                Additionally, I am proficient in{" "}
+                <span className="font-medium">TypeScript and Prisma.</span>{" "}
+                I&nbsp;am always eager to expand my knowledge by exploring new technologies 
+                and learning opportunities.
+            </p>
+            <p>
+                Outside of coding, I find enjoyment in playing video games, watching movies, 
+                and going for a jog. I am also passionate about continuous learning and am currently studying {" "}
+                <span className="font-medium">history, economics, and the dynamics of financial markets.</span>
             </p>
         </motion.section>
     )
