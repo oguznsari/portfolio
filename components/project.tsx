d5905249643a0ec78178e9c725c340ee72975449
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-09-21 07:30:44 +0300

    responsiveness fix for smaller devices

--- components/project.tsx
+++ components/project.tsx
@@ -35,12 +35,12 @@ const Project = ({
       <a href={link} target="_blank">
         <section
           className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden 
-                sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8
+                sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8
                 rounded-lg dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
         >
           <div
             className="sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full
-                pt-4 pb-7 px-5 group-even:ml-[18rem]"
+                pt-4 pb-7 px-5 sm:group-even:ml-[18rem]"
           >
             <h3 className="text-2xl font-semibold">{title}</h3>
             <p className="my-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -63,7 +63,7 @@ const Project = ({
             src={imageUrl}
             alt={title}
             quality={95}
-            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
                 group-even:right-[initial] group-even:-left-40 
                 transition group-hover:scale-105
                 group-hover:-translate-x-3 
