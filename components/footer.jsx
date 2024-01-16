74b0f17729693824231b00933a8dca79c05a78de
Oğuzhan SARI <oguzn.sari@gmail.com>
2024-01-16 19:03:25 +0300

    footer style bolder

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ const thisYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500 font-mono">
+    <footer className="mb-10 px-4 text-center text-gray-500 font-mono font-bold">
       <small className="mb-2 text-xs flex items-center justify-center gap-2">
         <FaRegCopyright />
         {thisYear} Oğuzhan SARI
